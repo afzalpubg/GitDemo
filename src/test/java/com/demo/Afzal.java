72eba01f768d065ec0fe666dd18428b5b7217188
afzal <readlearnalways@gmail.com>
2020-05-31 00:44:45 +0530

    first commit

---
+++ src/test/java/com/demo/Afzal.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class Afzal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("rex");
+		
+		System.out.println("red");
+	}
+
+}
